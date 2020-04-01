dea3a93635fdbd7fe0d39006fdcd7bfba5716085
Kleber <klebernascimento@outlook.com.br>
2020-04-01 20:21:36 -0300

    usando selenium grid (Selenium Server)

--- src/main/java/core/DriverFactory.java
+++ src/main/java/core/DriverFactory.java
@@ -4,6 +4,11 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DriverFactory {
 
@@ -13,20 +18,35 @@ public class DriverFactory {
 			return initDriver();
 		}
 	};
-	
+
 	private DriverFactory() {}
-	
+
 	public static WebDriver getDriver(){
 		return threadDriver.get();
 	}
-	
+
 	public static WebDriver initDriver(){
 		WebDriver driver = null;
-		switch (Propriedades.browser) {
-			case FIREFOX: driver = new FirefoxDriver(); break;
-			case CHROME: driver = new ChromeDriver(); break;
+		if(Propriedades.TIPO_EXECUCAO == Propriedades.TipoExecucao.LOCAL) {
+			switch (Propriedades.BROWSER) {
+				case FIREFOX: driver = new FirefoxDriver(); break;
+				case CHROME: driver = new ChromeDriver(); break;
+			}
 		}
-		driver.manage().window().setSize(new Dimension(1200, 765));			
+		if(Propriedades.TIPO_EXECUCAO == Propriedades.TipoExecucao.GRID) {
+			DesiredCapabilities cap = null;
+			switch (Propriedades.BROWSER) {
+				case FIREFOX: cap = DesiredCapabilities.firefox(); break;
+				case CHROME: cap = DesiredCapabilities.chrome(); break;
+			}
+			try {
+				driver = new RemoteWebDriver(new URL("http://192.168.42.125:4444/wd/hub"), cap);
+			} catch (MalformedURLException e) {
+				System.err.println("Falha na conex達o com o GRID");
+				e.printStackTrace();
+			}
+		}
+		driver.manage().window().setSize(new Dimension(1200, 765));
 		return driver;
 	}
 
